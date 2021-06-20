c128e54f016bd4db7a6a09ace87952e274857751
fabioasr89 <fabiopetricola@alice.it>
2021-06-20 13:46:43 +0200

    commit lotteria c sharp

---
+++ LotteriaConsoleApplication/Lotteria.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LotteriaConsoleApplication
+{
+    class Lotteria
+    {
+        public Lotteria() { }
+        public  int[,] generaMatriceCasualeUnivoca(Random Rand) {
+            int Casuale = 0;
+            int[,] numeriGenerati = new int[10, 10];
+            int[] lista = new int[100];
+            int Riga = 0;
+            int Colonna = 0;
+            for (int i=0;i<100;) {
+                int j = 0;
+                Casuale = Rand.Next(1, 200);
+                int semaforo = 0;
+                for (j=0;j<i && j>=0;j++)
+                    {
+                        if (Casuale == lista[j])
+                        {
+                            //significa che il numero casuale appena generato, è stato già generato in precedenza
+                            //usciamo dal ciclo e rigeneriamo di nuovo lo pseudocasuale
+                            semaforo++;
+                            break;
+                        }
+                           
+                }
+                if (semaforo == 0)
+                {
+                    //se il numero casuale non è stato già generato in precedenza, associamolo alla riga e incrementiamo il contatore
+                    lista[i] = Casuale;
+                    i++;
+                    //popolo la matrice 10x10 inizialmente vuota
+                    if(Riga<10 && Colonna < 10)
+                    {
+                        numeriGenerati[Riga, Colonna] = Casuale;
+                    }
+                    if (Colonna < 10)
+                    {
+                        Colonna++;
+                    }
+                    if (Colonna > 9)
+                    {
+                        Riga++;
+                        Colonna = 0;
+                    }
+                }
+            }
+
+            return numeriGenerati;
+
+        }
+
+
+        public  Boolean validaInserimento(int riga, int colonna,int numeroGenerato, int[,] listaInteri) {
+            Boolean inserisci = true;
+            if(riga==0 && colonna==0 && listaInteri != null)
+            {
+                return inserisci ;
+            }
+            else if((riga!=0 || colonna!=0) && listaInteri != null)
+            {
+                int Numero = 0;
+                for(int i=riga;i>=0 && i <= riga; i--)
+                {
+                    int j = colonna;
+                    for(j=colonna;j>=0 && j <=colonna; j--)
+                    {
+                        //dalla posizione i,j a cui dobbiamo assegnare uno pseudocasuale, scorriamo da 0 a j le colonne
+                        //per verificare che non sia presente già un numero uguale a quello generato. Fatto il controllo sulla riga i-esima,
+                        //si procede per iterazione sulla riga i-1 e la colonne da 0 a j, fino ad arrivare alla riga i=0 (la prima)
+                        //se troviamo un numero uguale a quello pseudocasuale già generato,usciamo dal ciclo e rigeneriamo il numero
+                        Numero = listaInteri[i, j];
+                        if (Numero == numeroGenerato)
+                        {
+                            inserisci = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                inserisci = false;
+            }
+            return inserisci;
+        
+        }
+    }
+}
---
+++ LotteriaConsoleApplication/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace LotteriaConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lotteria lotteria = new Lotteria();
+            Random Random = null;
+            int[,] Numeri = new int[10, 10];
+            string NumeroInserito = null;
+            int[] NumeriDigitati = new int[3];
+            int Estrazioni = 2;
+            try
+            {
+                Random = new Random();
+                Numeri = lotteria.generaMatriceCasualeUnivoca(Random);
+
+                Console.WriteLine("Matrice casuale generata");
+                int Count = 0;
+                do
+                {
+                    Console.WriteLine("Digita il numero:");
+                    NumeroInserito = Console.ReadLine();
+                    if (NumeroInserito != null)
+                    {
+                        try
+                        {
+                            int n = int.Parse(NumeroInserito);
+                            if (Count < 3)
+                            {
+                                NumeriDigitati[Count] = n;
+                                Count++;
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            Estrazioni = -1;
+                            Console.WriteLine("Non hai inserito un numero. Il gioco è finito");
+                        }
+                        Estrazioni--;
+                    }
+                    else
+                    {
+                        Estrazioni = -1;
+                    }
+                } while (Estrazioni >= 0);
+                Count = 0;
+                if (NumeriDigitati.Length > 0)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            int VALORE = Numeri[i, j];
+                            for (int k = 0; k < 3; k++)
+                            {
+                                if (VALORE == NumeriDigitati[k])
+                                {
+                                    Console.WriteLine("Il numero " + VALORE + " " + "è presente nella matrice pseuodocasuale");
+                                    Count++;
+                                }
+                            }
+                        }
+                    }
+                    Console.WriteLine("Numeri digitati:");
+                    for (int i = 0; i < 3; i++)
+                    {
+                        Console.Write(NumeriDigitati[i]);
+                        if (i <= 2)
+                        {
+                            Console.Write(",");
+                        }
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Matrice pseudocasuale generata:");
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.Write(Numeri[i, j]);
+                        if (j < 9)
+                        {
+                            Console.Write(",");
+                        }
+
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("********************");
+
+
+                Console.WriteLine("Hai indovinato " + Count + " " + "numeri dell'estrazione");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
